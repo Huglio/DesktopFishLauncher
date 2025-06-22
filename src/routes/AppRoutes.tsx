0681f0853c49a02e2154838af192ccc891d5b7f5
daniel <dan201619@gmail.com>
2025-06-21 22:06:58 -0300

    feat: update AppRoutes to include Introduction, ValidateInvite, and Activation pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import IntroductionPage from '../pages/Introduction';
+import ValidateInvitePage from '../pages/ValidateInvite';
+import ActivationPage from '../pages/Activation';
 import HomePage from '../pages/Home';
-import InvitePage from '../pages/Invite';
+import CreateInvitePage from '../pages/CreateInvite';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route>
+        <Route path="/introduction" element={<IntroductionPage />} />
+        <Route path="/validate-invite" element={<ValidateInvitePage />} />
+        <Route path="/activation" element={<ActivationPage />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="/invite" element={<InvitePage />} />
+        <Route path="/create-invite" element={<CreateInvitePage />} />
       </Route>
     </Routes>
   );
