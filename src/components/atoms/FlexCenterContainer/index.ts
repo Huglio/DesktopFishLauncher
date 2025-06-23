8be0840b86649216ebe83f9c8b5815687939a7e7
daniel <dan201619@gmail.com>
2025-06-23 18:52:46 -0300

    refactor: update FlexCenterContainer styles to remove justify-content and adjust margin-top

--- src/components/atoms/FlexCenterContainer/index.ts
+++ src/components/atoms/FlexCenterContainer/index.ts
@@ -3,7 +3,10 @@ import styled from 'styled-components';
 export const FlexCenterContainer = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  min-height: 100vh;
+  height: 100vh;
+
+  & > *:first-child {
+    margin-top: ${props => props.theme.spacing.lg};
+  }
 `;
