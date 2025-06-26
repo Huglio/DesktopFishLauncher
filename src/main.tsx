8dc41b9fa10758282c7b8da18cd50bca6bacf71a
daniel <dan201619@gmail.com>
2025-06-26 17:39:34 -0300

    feat: wrap application in SessionProvider for session management

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,19 @@ import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/global.ts';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme.ts';
+import { SessionProvider } from './contexts/SessionContext/index.tsx';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <AppRoutes />
+        <SessionProvider>
+          <GlobalStyle />
+          <AppRoutes />
+          <ToastContainer />
+        </SessionProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
